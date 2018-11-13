cafaa3fb937489c721ccab9e675f72fb1e7c0bbd
DAVID BARLOW <davidrbarlow@DAVIDs-MacBook-Pro-4.local>
2018-11-13 10:30:50 -0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {User} = require('./models/user')
+var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -38,6 +39,22 @@ app.get('/todos',(req,res) => {
 
 app.listen(3000, () =>{
     console.log('Started on port 3000')
+});
+
+// GET /todos/
+app.get('/todos/:id', (req, res)=>{
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo)=>{
+    if (!todo){
+      res.status(404).send();
+    } else
+    {res.send({todo})};
+  }, (e) =>{
+      res.status(400).send();
+  });
 });
 
 module.exports = {app};
