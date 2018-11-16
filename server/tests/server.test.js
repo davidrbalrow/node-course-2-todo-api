d7e88bdcca2ab699c73deee713873e1dc924f8a3
DAVID BARLOW <davidrbarlow@DAVIDs-MacBook-Pro-4.local>
2018-11-16 11:28:29 -0800

    Add tests for POST /user/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -248,17 +248,67 @@ describe('POST /users', () => {
         expect(user).toExist();
         expect(user.password).toNotBe(password);
         done();
-      });
+      }).catch((e)=> done(e));
     });
   });
 
-  it('should return validation errors if request invalid', (done)=>{
+  // it('should return validation errors if request invalid', (done)=>{
+  //
+  // });
+  //
+  // it('should not create user if email in use', (done)=>{
+  //
+  // });
+
+
+});
+
+
+describe('POST /users/login', () =>{
+  it('should login user and reurn auth token', (done)=>{
+    request(app)
+    .post('/users/login')
+    .send({
+      email:users[1].email,
+      password: users[1].password
+    })
+    .expect(200)
+    .expect((res)=>{
+      expect(res.headers['x-auth']).toExist();
+    })
+    .end((err, res) => {
+      if (err){
+        return done(err);
+      }
+      User.findById(users[1]._id).then((user)=>{
+        expect(user.tokens[0]).toInclude({
+          access: 'auth',
+          token: res.header['x-auth']
+        });
+        done();
+      }).catch((e)=> done(e));
+    });
+  });
+  it('should reject invalid login', (done)=>{
+    request(app)
+    .post('/users/login')
+    .send({
+      email:users[1].email,
+      password: 'asdf'
+    })
+    .expect(400)
+    .expect((res)=>{
+      expect(res.headers['x-auth']).toNotExist();
+    })
+    .end((err, res) => {
+      if (err){
+        return done(err);
+      }
+      User.findById(users[1]._id).then((user)=>{
+        expect(user.tokens.length).toBe(0);
+        done();
+      }).catch((e)=> done(e));
+    });
 
   });
-
-  it('should not create user if email in use', (done)=>{
-
-  });
-
-
 });
