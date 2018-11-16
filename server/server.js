53399b3334591f3de4028bad7e5d2cdd1376901a
DAVID BARLOW <davidrbarlow@DAVIDs-MacBook-Pro-4.local>
2018-11-16 12:31:48 -0800

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -129,6 +129,16 @@ app.get('/users/me', authenticate, (req, res) =>{
 
 });
 
+app.delete('/users/me/token', authenticate, (req,res) => {
+  console.log("test");
+  console.log("req");
+  req.user.removeToken(req.token).then(()=>{
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
 app.post('/users/login', (req, res) =>{
   var body = _.pick(req.body, ['email', 'password']);
 
